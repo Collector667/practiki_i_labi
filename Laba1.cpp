6a062a0b5b1de9ca2b18003fe7b1a89190305503
Collector667 <146832679+Collector667@users.noreply.github.com>
2023-10-09 22:39:26 +0300

    vse

--- Laba1.cpp
+++ Laba1.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <stdio.h>
 #include <conio.h>
 #include <clocale>
@@ -8,39 +8,50 @@
 int main() {
 	using namespace std;
 	setlocale(LC_ALL, "RU");
-	int chislo1, chislo2, border, border1, schislen = 10;
-	border = 1000; //внешняя граница
-	border1 = 100; //внутренная граница
+	int chisloVhod, limitTri, schislen = 10, razryad = 3;
+	int  chisloVih = 0;
+	limitTri = pow(schislen, razryad); // Введите нужный разряд
 	cout << "Введите трехзначное число: ";
-	std::cin >> chislo1;
-	if ((chislo1 < border) and (chislo1 > -border) and ((chislo1 >= border1) or (chislo1 <= -border1))) {
-		int num1, num2, num3, minChislo, count, proverka;
-		string vihZnak;
+	std::cin >> chisloVhod;
+	if ((abs(chisloVhod) < (schislen * limitTri) and ((abs(chisloVhod) >= (limitTri / schislen))))) {
+
+		bool vihZnak = 1;
 		std::list<int> listNums;
-		if (chislo1 < 0) {
-			chislo1 = -chislo1;
-			vihZnak = "-";
+		if (chisloVhod < 0) {
+			chisloVhod = -chisloVhod;
+			vihZnak = 0;
 		}
 
-		while (chislo1 > 0) {
-			listNums.push_back(chislo1 % schislen);
-			chislo1 = chislo1 / schislen;
+		while (chisloVhod > 0) {
+			listNums.push_back(chisloVhod % schislen);
+			chisloVhod = chisloVhod / schislen;
 		}
-		std::cout << "Выходное число: " << vihZnak;
-		for (int i : listNums) {
-			cout << i;
+
+		std::cout << "Выходное число: ";
+		int size = listNums.size();
+		for (int i = 1; i <= size; i++) {
+			chisloVih += listNums.front() * pow(schislen, (razryad - i));
+
+			listNums.pop_front();
+
+		}
+		if (vihZnak == 0) {
+			chisloVih = -chisloVih;
+			printf_s("%04d", chisloVih);
+		}
+		else {
+			printf_s("%03d", chisloVih);
 		}
 	}
 	else {
 		std::cout << "Вы ввели не трехзначное число";
 	}
-	int chislo3, chislo4 = 1, border2, border3;
+	int chislo3, chislo4 = 1, razryadChe = 4, limitChe;
 	std::cout << " \nВведите четырехзначное число: ";
 	std::cin >> chislo3;
-	border2 = 10000; //внешняя граница
-	border3 = 1000; //внутренная граница
+	limitChe = pow(schislen, razryadChe);
 	chislo3 = sqrt(chislo3 * chislo3);
-	if ((chislo3 >= border3) and (chislo3 < border2))  {
+	if ((chislo3 >= (limitChe / schislen) and (chislo3 < limitChe * schislen))) {
 		std::list<int> listNumss;
 		while (chislo3 > 0) {
 			listNumss.push_back(chislo3 % schislen);
---
+++ labaokruz.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <clocale>
+#include <windows.h>
+#include <math.h>
+#include <list>
+int main() {
+	setlocale(LC_ALL, "RU");
+	using namespace std;
+	float coorX, coorY, radius, radiusVec;
+	int chetvert = 0;
+	cout << "Введите радиус окружности: ";
+	cin >> radius;
+	cout << "\nВведите координату по Х: ";
+	cin >> coorX;
+	cout << "\nВведите координату по У: ";
+	cin >> coorY;
+	radiusVec = sqrt(coorX * coorX + coorY * coorY);
+	if (radius >= 0) {
+		if ((((coorX >= 0) and (coorY >= 0)) and ((coorX == 0) or ((coorY == 0) and (radius <= radiusVec)) or
+			(((coorX <= 0) and (coorY >= 0)) and ((coorX == 0) or ((coorY == 0) and (radiusVec <= radius)))) or
+			((((coorX <= 0) and (coorY <= 0)) and ((radius == radiusVec) or (((coorX == 0) or (coorY == 0) and (radius > radiusVec)))))) or
+			((((coorX >= 0) and (coorY <= 0)) and ((radius == radiusVec) or (((coorX == 0) or (coorY == 0) and (radius > radiusVec))))))))) {
+			cout << "Точна на границе";
+		}
+		else if ((((coorY == 0) and (radius > radiusVec)) or ((coorX > 0) and (coorY > 0)) or (((coorX < 0) and (coorY < 0)) and (radius > radiusVec))) or
+			(((coorX > 0) and (coorY < 0)) and (radius > radiusVec))) {
+			cout << "Точка в области";
+
+		}
+		else {
+			cout << "Точка вне области";
+		}
+
+
+
+
+	}
+	else {
+		cout << "Радиус не может быть отрицательным";
+	}
+	
+	return 0;
+}
